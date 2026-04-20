31fb7472d97f250dfdfdf6f8bc2a3a677391a16c
shuaib khan <shuaib.khan@unitedsol.net>
2026-04-19 23:01:46 -0700

    updated sitemap.xml

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,10 +3,16 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://shuaibkhan.dev',
+      url: 'https://www.welivesoft.com/',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
+    },
+    {
+      url: 'https://www.welivesoft.com/Shuaibkhan_Resume.pdf',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
     },
   ]
 }
