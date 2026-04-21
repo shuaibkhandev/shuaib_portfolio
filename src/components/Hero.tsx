138d3be012e3fdc0783335251860b1c1fae9d3b6
shuaib khan <shuaib.khan@unitedsol.net>
2026-04-21 12:37:02 +0500

    removed test

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -26,7 +26,7 @@ export default function Hero() {
               <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
               <span className="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
             </span>
-            Available for worksss
+            Available for work
           </div>
         </motion.div>
 
